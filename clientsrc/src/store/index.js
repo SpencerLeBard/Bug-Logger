8c6d2a169082dd5fb07122465cd87036db176e45
SpencerLeBard <slebard@gmail.com>
2020-09-30 16:03:08 -0600

    finalfinal

--- clientsrc/src/store/index.js
+++ clientsrc/src/store/index.js
@@ -1,48 +1,46 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import router from "../router";
-import { api } from "./AxiosService"
-import ns from "../Services/NotificationService"
+import { api } from "./AxiosService";
+import ns from "../Services/NotificationService";
 
 Vue.use(Vuex);
-
-
 
 export default new Vuex.Store({
   state: {
     profile: {},
     bugs: [],
     activeBug: {},
-    notes: []
+    notes: [],
   },
   mutations: {
     setProfile(state, profile) {
       state.profile = profile;
     },
     setBugs(state, bugs) {
-      state.bugs = bugs
+      state.bugs = bugs;
     },
     createBug(state, bug) {
-      state.bugs.push(bug)
+      state.bugs.push(bug);
     },
     closeBug(state, bug) {
-      state.bugs = bug
+      state.bugs = bug;
     },
     setActiveBug(state, bug) {
-      state.activeBug = bug
+      state.activeBug = bug;
     },
     setNotes(state, notes) {
-      state.notes = notes
+      state.notes = notes;
     },
     createNote(state, note) {
-      state.notes = note
+      state.notes = note;
     },
     removeNote(state, note) {
-      state.notes = state.notes.filter(n => n.id != note)
-    }
+      state.notes = state.notes.filter((n) => n.id != note);
+    },
   },
   actions: {
-    setBearer({ }, bearer) {
+    setBearer({}, bearer) {
       api.defaults.headers.authorization = bearer;
     },
     resetBearer() {
@@ -58,39 +56,38 @@ export default new Vuex.Store({
     },
     async getBugs({ commit }) {
       try {
-        let res = await api.get('bugs')
-        commit('setBugs', res.data)
-      } catch (error) {
-
-      }
+        let res = await api.get("bugs");
+        commit("setBugs", res.data);
+      } catch (error) {}
     },
     async addBug({ commit }, bugData) {
       try {
-        let res = await api.post('bugs', bugData)
-        commit("createBug", res.data)
-        router.push({ path: "bugs/" + res.data.id })
+        let res = await api.post("bugs", bugData);
+        commit("createBug", res.data);
+        router.push({ path: "bugs/" + res.data.id });
         this.dispatch("getActiveBug", res.data.id);
         this.dispatch("getNotes", bugData);
-
-      } catch (error) {
-
-      }
+      } catch (error) {}
     },
 
     async closeBug({ commit }, bugId) {
       try {
-        if (await ns.confirmAction("Do you want to close this bug?", "It'll be squashed forever ...")) {
-          await api.delete('bugs/' + bugId)
-        } commit("closeBug", bugId)
-        commit("setActiveBug", bugId)
-      } catch (error) {
-
-      }
+        if (
+          await ns.confirmAction(
+            "Do you want to close this bug?",
+            "It'll be squashed forever ..."
+          )
+        ) {
+          await api.delete("bugs/" + bugId);
+        }
+        commit("closeBug", bugId);
+        commit("setActiveBug", bugId);
+      } catch (error) {}
     },
     async getActiveBug({ commit }, bugId) {
       try {
-        let res = await api.get('bugs/' + bugId)
-        commit('setActiveBug', res.data)
+        let res = await api.get("bugs/" + bugId);
+        commit("setActiveBug", res.data);
         // @ts-ignore
         this.dispatch("getNotes", bugId);
       } catch (error) {
@@ -100,27 +97,31 @@ export default new Vuex.Store({
     async getNotes({ commit }, bug) {
       try {
         // console.log(bug);
-        let res = await api.get('bugs/' + bug + '/notes')
-        // console.log(res);
+        let res = await api.get("bugs/" + bug + "/notes");
         // let res = await api.get('notes/')
-        commit('setNotes', res.data)
+        commit("setNotes", res.data);
       } catch (error) {
         console.error(error);
       }
     },
     async addNote({ commit, state }, noteData) {
       try {
-        let res = await api.post('notes', noteData)
-        commit("createNote", [...state.notes, res.data])
+        let res = await api.post("notes", noteData);
+        commit("createNote", [...state.notes, res.data]);
       } catch (error) {
         console.error(error);
       }
     },
     async deleteNote({ commit }, noteId) {
       try {
-        if (await ns.confirmAction("Do you want to delete this Note?", "You'll never get it back ...")) {
-          await api.delete('notes/' + noteId)
-          commit("removeNote", noteId)
+        if (
+          await ns.confirmAction(
+            "Do you want to delete this Note?",
+            "You'll never get it back ..."
+          )
+        ) {
+          await api.delete("notes/" + noteId);
+          commit("removeNote", noteId);
         }
       } catch (error) {
         console.error(error);
@@ -128,11 +129,11 @@ export default new Vuex.Store({
     },
     async editBug({ commit }, bugData) {
       try {
-        let res = await api.put('bugs/' + bugData.id, bugData)
-        commit('setActiveBug', res.data)
+        let res = await api.put("bugs/" + bugData.id, bugData);
+        commit("setActiveBug", res.data);
       } catch (error) {
         console.error(error);
       }
     },
-  }
+  },
 });
